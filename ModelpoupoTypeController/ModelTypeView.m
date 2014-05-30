61d93aaaa9d05b9bb3cccd2caebb584f7697db11
david <liuchong1228@126.com>
2014-05-30 19:08:03 +0800

    ddd

--- ModelpoupoTypeController/ModelTypeView.m
+++ ModelpoupoTypeController/ModelTypeView.m
@@ -7,11 +7,12 @@
 //
 
 #import "ModelTypeView.h"
+#define IOS7 ([UIDevice currentDevice].systemVersion.floatValue >= 7.000)
+#define IOS568 ([[UIScreen mainScreen] bounds].size.height >= 568)
 #define INPUTVIEW_MAX_HEIGHT 300
 #define kscreenHeight ([[UIScreen mainScreen] bounds].size.height)
 @interface ModelTypeView()
 @property (nonatomic,strong) UIViewController *controller;
-@property (weak, nonatomic) IBOutlet UILabel *charNumberTipLabel;
 @property (nonatomic,strong) UIWindow *presentWindow;
 @property (nonatomic,strong) void(^finishedInput)(NSString *string);
 @property (nonatomic,assign) BOOL isFinished;
@@ -22,6 +23,7 @@
 @property (weak, nonatomic) IBOutlet UITextView *textView;
 @property (weak, nonatomic) IBOutlet UIButton *backGroundButton;
 
+@property (nonatomic,strong) NSString *tipString;
 @property (nonatomic,strong) NSNumber *maxLength;
 - (IBAction)OKButtonClicked:(id)sender;
 
@@ -54,7 +56,7 @@
     controller.textView.layer.cornerRadius = 5;
     controller.textView.layer.borderColor = [UIColor lightGrayColor].CGColor;
     controller.inputBackView.backgroundColor = [UIColor whiteColor];
-    
+    controller.tipString = tip;
     controller.controller = parentController;
     controller.cancel = cancel;
     controller.finishedInput = finished;
@@ -71,51 +73,35 @@
     
     [controller.textView becomeFirstResponder];
     controller.maxLength = maxLength;
-    controller.charNumberTipLabel.text = [NSString stringWithFormat:@"0/%d",maxLength.integerValue];
 }
 #pragma mark uitextviewDelegate
+
 -(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text{
-    NSLog(@"%@ >>>>%@>>",textView.text,text);
     if ([text isEqualToString:@" "]|| [text isEqualToString:@"\n"]) {
         return NO;
     }
     if (CGRectGetHeight(self.inputBackView.frame) < INPUTVIEW_MAX_HEIGHT) {
-        float height = textView.contentSize.height - self.textViewContentHeight-9;
+        float height = textView.contentSize.height - self.textViewContentHeight;
         height = height > 0 ?height:0;
         self.textViewContentHeight = textView.contentSize.height;
         CGRect rect =self.inputBackView.frame;
         self.inputBackView.frame = (CGRect){rect.origin.x,rect.origin.y-height,rect.size.width,rect.size.height+height};
     }
-    if ([text isEqualToString:@""]) {
-        self.charNumberTipLabel.text = [NSString stringWithFormat:@"%d/%d",textView.text.length <= 0?0:textView.text.length-1,self.maxLength.intValue];
-    }else{
-        self.charNumberTipLabel.text = [NSString stringWithFormat:@"%d/%d",textView.text.length >= self.maxLength.integerValue ?self.maxLength.integerValue:textView.text.length+1,self.maxLength.intValue];
-    }
-    if (!self.maxLength) {
-        return YES;
-    }else{
-        if (textView.text.length >= self.maxLength.intValue) {
-            if ([text isEqualToString:@""]) {
-                return YES;
-            }
-            return NO;
-        }else{
-            return YES;
-        }
-    }
+    return YES;
 }
 
 -(void)textViewDidBeginEditing:(UITextView *)textView{
     self.textViewContentHeight = textView.contentSize.height;
 }
+
 #pragma mark --
 
 
 -(void)keyBoardUP:(NSNotification*)notification{
     NSTimeInterval animationDuration = [[notification.userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue];
     CGRect rect = [[notification.userInfo objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
-    float height = rect.size.height;
-    self.inputBackView.frame = (CGRect){0,kscreenHeight,CGRectGetWidth(self.frame),120};
+    float height = IOS7?rect.size.height:rect.size.height+20;
+    self.inputBackView.frame = (CGRect){0,kscreenHeight,CGRectGetWidth(self.frame),130};
     [UIView animateWithDuration:animationDuration animations:^{
         self.inputBackView.frame = (CGRect){0,kscreenHeight-height-CGRectGetHeight(self.inputBackView.frame),CGRectGetWidth(self.frame),self.inputBackView.frame.size.height};
     } completion:^(BOOL finished) {
@@ -130,7 +116,7 @@
     [self.presentWindow setHidden:YES];
     self.presentWindow = nil;
     [UIView animateWithDuration:animationDuration animations:^{
-        self.inputBackView.frame = (CGRect){0,kscreenHeight,CGRectGetWidth(self.frame),120};
+        self.inputBackView.frame = (CGRect){0,kscreenHeight,CGRectGetWidth(self.frame),130};
     } completion:^(BOOL finished) {
         [[NSNotificationCenter defaultCenter] removeObserver:self];
         [self removeFromSuperview];
@@ -149,6 +135,18 @@
 
 - (IBAction)OKButtonClicked:(id)sender {
     self.isFinished = YES;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        self.tipLabel.text = self.tipString;
+    });
+    if ([self.textView.text isEqualToString:@""]) {
+        self.tipLabel.text = @"输入名称不能为空";
+        return;
+    }
+    if (self.textView.text.length > self.maxLength.intValue) {
+        self.tipLabel.text = [NSString stringWithFormat:@"输入名称长度不能超过%d个字符",self.maxLength.intValue];
+        return;
+    }
+    
     [self.textView resignFirstResponder];
 }
 
