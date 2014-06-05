f68c65ed1e97687323296a5981f7db7af9411887
david <liuchong1228@126.com>
2014-06-05 15:35:33 +0800

    提示字数超出限制错误重新输入无法输入

--- ModelpoupoTypeController/ModelTypeView.m
+++ ModelpoupoTypeController/ModelTypeView.m
@@ -194,6 +194,7 @@
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         self.tipLabel.text = self.tipString;
         self.textView.text = @"";
+        self.inTypeLength = 0;
     });
     if ([self.textView.text isEqualToString:@""]) {
         self.tipLabel.text = @"输入名称不能为空";
